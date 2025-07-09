99fbfb72d933925549bfbdbc6dc1d4c415ee768b
Aryna <fontesaryna@gmail.com>
2025-07-08 21:58:09 -0300

    Novas validações

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,24 +20,42 @@ const livros: Livro[] = [
     }
 ];
 
+// Rota GET para listar livros
 app.get('/livros', (req: any, res: any) => {
   res.json(livros);
 });
 
+// Rota POST para cadastrar um livro
 app.post('/livros', (req: any, res: any) => {
     const { nome, autor } = req.body;
+    if(!nome) {
+        res.status(500).json({error: "Nome do livro é obrigatório."})
+        return 
+    }
+
+    if(!autor) {
+        res.status(500).json({error: "Nome do autor é obrigatório."})
+        return
+    }
+
+    if(nome.length < 3) { 
+        res.status(500).json({error: "Nome não deve conter menos que três caracteres."})
+        return
+    }
+
+    if(autor.length < 3) {
+        res.status(500).json({error: "Autor não deve conter menos que três caracteres."})
+        return
+    }
+
     const novoLivro = {
     id: livros.length + 1,
     nome,
     autor
   };
 
-    console.log("Antes de cadastrar", livros)
-
-
     livros.push(novoLivro);
     res.status(201).json({ mensagem: 'Livro cadastrado com sucesso.', livro: novoLivro });
-    console.log("Depois de cadastrar", livros)
 })
 
 app.listen(port, () => {
